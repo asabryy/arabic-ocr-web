ee17dde0d50fead5334f138303f41b74400dbce5
Ahmed Sabry <ahmedsabry1996@hotmail.com>
2025-07-26 13:14:41 -0400

    update frontend base url

--- frontend/src/features/auth/authservice.js
+++ frontend/src/features/auth/authservice.js
@@ -1,2 +1,2 @@
 import axios from "axios";
-export const authApi = axios.create({ baseURL: "/api/auth/v1" });
+export const authApi = axios.create({ baseURL: "https://auth-service-k63w.onrender.com/api/auth/v1" });
